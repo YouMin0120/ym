b002012283e6bb97ce9e509b9ec36454ac5ed077
YouMin0120 <93131679+YouMin0120@users.noreply.github.com>
2021-10-26 20:39:10 +0900

    Add files via upload

---
+++ youmin2.py
@@ -0,0 +1,47 @@
+#a=int(input())
+#   while a<11:
+#       print("*"*a)
+#           a=a+1
+# for i in range():
+# a=1
+#  while a<11:
+#      print("*"*(10-a))
+#      a=a+1
+# for i in range(11):
+#     # print("*"*(10-i))
+#a=[숫자와 문자를 여러가지 넣을수 있음]
+#a.append(대괄호에 들어가는 문자 또는 숫자를 추가할수 있음)
+# a=int(input())
+# if a % 15 == 0:
+#     print("**")
+# elif a%5==0 or a%3==0:
+#     print("*")
+# a=[1,2,3,4]
+#x=[]
+#x[]
+#x.append()
+#
+# import random
+# x = []
+# for i in range(1,102):
+#     a=random.randrange(1,6)
+#     x.append(a)
+# print(x)
+# b=0
+# c=0
+# d=0
+# e=0
+# f=0
+# for i in range(1,101):
+#     x[i]
+#     if x[i]==1:
+#         b=b+1
+#     elif x[i]==2:
+#         c=c+1
+#     elif x[i] == 3:
+#         d=d+1
+#     elif x[i] == 4:
+#         e=e+1
+#     elif x[i] == 5:
+#         f=f+1
+# print(b,c,d,e,f)
