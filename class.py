50655d4d7cbd88fe56ae2df5d44b848e9e9598cc
YouMin0120 <93131679+YouMin0120@users.noreply.github.com>
2021-11-27 13:35:23 +0900

    Add files via upload

--- class.py
+++ class.py
@@ -23,12 +23,15 @@
 #             min = l[h]
 # print(l)
 import random
-l = []
-for j in range(5):
-    l.append(random.randrange(1, 101))
-    for k in range(len(l)):
-        min = k
-        for i in range(k, len(l)):
-            if a[min] > a[i]:
-            min = i
-        a[k], a[min] = a[min], a[k]
+def YM():
+    l = []
+    for j in range(5):
+        l.append(random.randrange(1, 101))
+        for k in range(len(l)):
+            min = k
+            for i in range(k, len(l)):
+                if l[min] > l[i]:
+                    min = i
+            l[k], l[min] = l[min], l[k]
+    print(l)
+YM()
