06488676c48ccd753805fc9251a32f118d349913
YouMin0120 <93131679+YouMin0120@users.noreply.github.com>
2021-12-04 12:17:44 +0900

    Create Rock Scissors Paper.py

---
+++ Game/Rock Scissors Paper.py
@@ -0,0 +1,17 @@
+# import random
+# def YM():
+#     return input()
+# def COM():
+#     return b[random.randrange(3)]
+# def GAME():
+#     if (x == "가위" and c == "가위") or (x == "바위" and c == "바위") or (x == "보" and c == "보"):
+#         print("비겼습니다")
+#     elif (x == "가위" and c == "바위") or (x == "바위" and c == "보") or (x == "보" and c == "가위"):
+#         print("패배하였습니다")
+#     elif (x == "바위" and c == "가위") or (x == "보" and c == "바위") or (x == "가위" and c == "보"):
+#         print("승리하였습니다")
+# x=YM()
+# b=["가위" , "바위" , "보"]
+# c=COM()
+# print(c)
+# GAME()
