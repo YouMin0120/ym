c588027638e232df5c9bf435393bca6a212979f9
YouMin0120 <93131679+YouMin0120@users.noreply.github.com>
2021-12-04 12:15:18 +0900

    Create multiplication table.py

---
+++ Game/multiplication table.py
@@ -0,0 +1,9 @@
+#for j in range(2, 10):
+#    for i in range(1,10):
+#        b = "{} * {} = {}".format(j , i , (j*i))
+#        print(b)
+
+#class GAME():
+#    def gm(self):
+#        print(1)
+#a = GAME()
